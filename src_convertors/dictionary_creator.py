c7357f67c931b691e4b7236a1f7bfa2d725245d8
gisly <gisly@list.ru>
2020-08-26 01:03:28 +0300

    скрипт, правящий глоссы

--- src_convertors/dictionary_creator.py
+++ src_convertors/dictionary_creator.py
@@ -97,23 +97,41 @@ def create_dictionary(folder):
     dictionary = generate_morpheme_dictionary(folder)
     print_dictionary(dictionary, output_filename)
 
+
 def correct_morphemes_glosses_in_file(filename, morphemes_replace, glosses_replace):
     srcTree = ET.parse(filename).getroot()
     morpheme_tier_element = srcTree.find('./TIER[@TIER_ID="' +
                                          MORPHEME_TIER_NAME + '"]')
     gloss_tier_element = srcTree.find('./TIER[@TIER_ID="' +
                                       GLOSS_TIER_NAME + '"]')
-    morphemes = morpheme_tier_element.findall('./ANNOTATION/ALIGNABLE_ANNOTATION/ANNOTATION_VALUE')
-    glosses = gloss_tier_element.findall('./ANNOTATION/ALIGNABLE_ANNOTATION/ANNOTATION_VALUE')
-    for i, morpheme_annotation in enumerate(morphemes):
-        gloss_annotation = glosses[i]
-        morphemes_text = morpheme_annotation.text
-        glosses_text = gloss_annotation.text
+    morphemes_words = morpheme_tier_element.findall('./ANNOTATION/ALIGNABLE_ANNOTATION/ANNOTATION_VALUE')
+    glosses_words = gloss_tier_element.findall('./ANNOTATION/ALIGNABLE_ANNOTATION/ANNOTATION_VALUE')
+    for i, morpheme_annotation in enumerate(morphemes_words):
+        gloss_annotation = glosses_words[i]
+        correct_morphemes_glosses_for_word(filename, morpheme_annotation.text, gloss_annotation.text, morphemes_replace, glosses_replace)
+
+def correct_morphemes_glosses_for_word(filename, morpheme_annotation, gloss_annotation, morphemes_replace, glosses_replace):
+    morphemes = morpheme_annotation.strip().split(' ')
+    glosses = gloss_annotation.strip().split(' ')
+
+
+
+    for i, morphemes_text in enumerate(morphemes):
+        glosses_text = glosses[i]
+        if 'ругаться-NPST' in glosses_text:
+            TODO = 9
         word_parts = re.split('[-]', morphemes_text.strip('='))
         gloss_parts = re.split('[-]', glosses_text.strip('='))
         word_corrected = ''
         gloss_corrected = ''
+
+        if len(word_parts) != len(gloss_parts):
+            print("!!!!!:" + filename + ":" + morphemes_text + ":" + glosses_text)
+            return
         for i, word_part in enumerate(word_parts):
+            morpheme_to_replace = None
+            gloss_to_replace = None
+
             if i >= len(gloss_parts):
                 print(filename, word_parts, gloss_parts)
                 continue
@@ -126,23 +144,31 @@ def correct_morphemes_glosses_in_file(filename, morphemes_replace, glosses_repla
             gloss_part_corrected = ''
             if len(word_parts_split_parts) == len(gloss_part_split_parts) and len(word_parts_split_parts) > 1:
                 for j, word_parts_split_part in enumerate(word_parts_split_parts):
+                    morpheme_to_replace = None
+                    gloss_to_replace = None
+
                     gloss_part_split_part = gloss_part_split_parts[j]
                     if morphemes_replace:
-                        to_replace = morphemes_replace.get((word_parts_split_part, gloss_part_split_part))
-                        if to_replace:
-                            word_part_corrected += '=' + to_replace
-                            gloss_part_corrected += '=' + gloss_part_split_part
-                        else:
-                            word_part_corrected += '=' + word_parts_split_part
-                            gloss_part_corrected += '=' + gloss_part_split_part
+                        morpheme_to_replace = morphemes_replace.get((word_parts_split_part, gloss_part_split_part))
+                    if glosses_replace:
+                        gloss_to_replace = glosses_replace.get((word_parts_split_part, gloss_part_split_part))
+
+                    if morpheme_to_replace:
+                        word_part_corrected +=  morpheme_to_replace + '='
                     else:
-                        word_part_corrected += '=' + word_parts_split_part
-                        gloss_part_corrected += '=' + gloss_part_split_part
+                        word_part_corrected += word_parts_split_part + '='
+                    if gloss_to_replace:
+                        gloss_part_corrected +=  gloss_to_replace + '='
+                    else:
+                        gloss_part_corrected +=  gloss_part_split_part + '='
+
+
+
             else:
                 if morphemes_replace:
-                    to_replace = morphemes_replace.get((word_part, gloss_part))
-                    if to_replace:
-                        word_part_corrected += '-' + to_replace
+                    morpheme_to_replace = morphemes_replace.get((word_part, gloss_part))
+                    if morpheme_to_replace:
+                        word_part_corrected += '-' + morpheme_to_replace
                         gloss_part_corrected += '-' + gloss_part
                     else:
                         word_part_corrected += '-' + word_part
@@ -151,13 +177,27 @@ def correct_morphemes_glosses_in_file(filename, morphemes_replace, glosses_repla
                     word_part_corrected += '-' + word_part
                     gloss_part_corrected += '-' + gloss_part
 
-            word_corrected += ' ' + word_part_corrected
-            gloss_corrected += ' ' + gloss_part_corrected
+            word_corrected += '-' + word_part_corrected
+            gloss_corrected += '-' + gloss_part_corrected
+            if word_corrected.endswith('='):
+                word_corrected = word_corrected[:-1]
+            if gloss_corrected.endswith('='):
+                gloss_corrected = gloss_corrected[:-1]
 
-        word_corrected = word_corrected.replace('--', '-').replace(' -', '-').strip().strip('-').strip('=')
-        gloss_corrected = gloss_corrected.replace('--', '-').replace(' -', '-').strip().strip('-').strip('=')
+        word_corrected = word_corrected.replace('--', '-').replace(' -', '-').strip().strip('-')
+        gloss_corrected = gloss_corrected.replace('--', '-').replace(' -', '-').strip().strip('-')
+        if word_corrected.startswith('='):
+            word_corrected = word_corrected[1:]
+        if gloss_corrected.startswith('='):
+            gloss_corrected = gloss_corrected[1:]
+        if morphemes_text.endswith('=') and not(word_corrected.endswith('=')):
+            word_corrected += '='
+        if glosses_text.endswith('=') and not(gloss_corrected.endswith('=')):
+            gloss_corrected += '='
         if word_corrected != morphemes_text:
-            print(morphemes_text, '---->', word_corrected)
+            print('morphemes', morphemes_text, '---->', word_corrected)
+        if gloss_corrected != glosses_text:
+            print('glosses', glosses_text, '---->', gloss_corrected)
 
 
 
@@ -193,6 +233,6 @@ def read_dictionary(filename):
 
     return morphemes_replace, glosses_replace
 
-folder = "D:/Елена/mansi_corpus/corpus/mansi/eaf"
-create_dictionary(folder)
+folder = "D://Mansi//Corpus//corpus//mansi//preprocessing"
+#create_dictionary(folder)
 correct_morphemes_glosses(folder, "dictionary_corrected.csv")
